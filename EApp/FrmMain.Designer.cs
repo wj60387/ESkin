72a2cec39b3e40ae4d65f5b0c1cf200c8c53f83d
wanjian <469370972@qq.com>
2016-12-23 20:41:43 +0800

    q

--- EApp/FrmMain.Designer.cs
+++ EApp/FrmMain.Designer.cs
@@ -42,7 +42,7 @@
             this.toolStripEx3.AutoSize = false;
             this.toolStripEx3.BackColor = System.Drawing.Color.Yellow;
             this.toolStripEx3.BackgroundImage = global::EApp.Properties.Resources.导航栏200x1080;
-            this.toolStripEx3.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStripEx3.Dock = System.Windows.Forms.DockStyle.Left;
             this.toolStripEx3.GripMargin = new System.Windows.Forms.Padding(0);
             this.toolStripEx3.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
             this.toolStripEx3.ImageScalingSize = new System.Drawing.Size(48, 48);
@@ -53,72 +53,77 @@
             this.toolStripButton9,
             this.toolStripButton5});
             this.toolStripEx3.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.VerticalStackWithOverflow;
-            this.toolStripEx3.Location = new System.Drawing.Point(87, 52);
+            this.toolStripEx3.Location = new System.Drawing.Point(0, 0);
             this.toolStripEx3.MouseMoveItem = null;
             this.toolStripEx3.Name = "toolStripEx3";
             this.toolStripEx3.Padding = new System.Windows.Forms.Padding(0);
             this.toolStripEx3.PressItem = null;
             this.toolStripEx3.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
-            this.toolStripEx3.Size = new System.Drawing.Size(93, 395);
+            this.toolStripEx3.Size = new System.Drawing.Size(122, 598);
             this.toolStripEx3.TabIndex = 6;
             this.toolStripEx3.Text = "toolStripEx3";
             // 
             // toolStripButton3
             // 
             this.toolStripButton3.Image = global::EApp.Properties.Resources.听诊器配置;
+            this.toolStripButton3.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton3.Margin = new System.Windows.Forms.Padding(0);
             this.toolStripButton3.Name = "toolStripButton3";
-            this.toolStripButton3.Size = new System.Drawing.Size(92, 69);
+            this.toolStripButton3.Size = new System.Drawing.Size(121, 116);
             this.toolStripButton3.Text = "AAA";
             this.toolStripButton3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             // 
             // toolStripButton4
             // 
             this.toolStripButton4.Image = global::EApp.Properties.Resources.听诊教学;
+            this.toolStripButton4.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton4.Margin = new System.Windows.Forms.Padding(0);
             this.toolStripButton4.Name = "toolStripButton4";
-            this.toolStripButton4.Size = new System.Drawing.Size(92, 69);
+            this.toolStripButton4.Size = new System.Drawing.Size(121, 107);
             this.toolStripButton4.Text = "BBB";
             this.toolStripButton4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             // 
             // toolStripButton8
             // 
             this.toolStripButton8.Image = global::EApp.Properties.Resources.听诊录音;
+            this.toolStripButton8.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.toolStripButton8.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton8.Margin = new System.Windows.Forms.Padding(0);
             this.toolStripButton8.Name = "toolStripButton8";
-            this.toolStripButton8.Size = new System.Drawing.Size(92, 69);
+            this.toolStripButton8.Size = new System.Drawing.Size(121, 109);
             this.toolStripButton8.Text = "CCC";
             this.toolStripButton8.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             // 
             // toolStripButton9
             // 
             this.toolStripButton9.Image = global::EApp.Properties.Resources.云端听诊;
+            this.toolStripButton9.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.toolStripButton9.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton9.Margin = new System.Windows.Forms.Padding(0);
             this.toolStripButton9.Name = "toolStripButton9";
-            this.toolStripButton9.Size = new System.Drawing.Size(92, 69);
+            this.toolStripButton9.Size = new System.Drawing.Size(121, 98);
             this.toolStripButton9.Text = "DDD";
             this.toolStripButton9.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             // 
             // toolStripButton5
             // 
-            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
             this.toolStripButton5.Image = global::EApp.Properties.Resources.远程教学;
+            this.toolStripButton5.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
             this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton5.Margin = new System.Windows.Forms.Padding(0);
             this.toolStripButton5.Name = "toolStripButton5";
-            this.toolStripButton5.Size = new System.Drawing.Size(92, 52);
-            this.toolStripButton5.Text = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(121, 101);
+            this.toolStripButton5.Text = "EEE";
+            this.toolStripButton5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(241)))), ((int)(((byte)(249)))));
-            this.ClientSize = new System.Drawing.Size(624, 557);
+            this.ClientSize = new System.Drawing.Size(811, 598);
             this.Controls.Add(this.toolStripEx3);
             this.MaximumSize = new System.Drawing.Size(1366, 728);
             this.Name = "FrmMain";
